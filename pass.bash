da47631736f367f74d53211113631afff176927f
rsiri <48105657+romaric-siri@users.noreply.github.com>
2024-01-02 21:15:33 +0100

    Update pass.bash

--- pass.bash
+++ pass.bash
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-json_data=$(bw list items --folderid SSH_FOLDER_ID | jq)
+# Factorized command to get the Bitwarden items JSON
+json_data=$(bw list items --folderid FOLDER_UID | jq)
 
-# Extract the list of usernames from the JSON
-usernames=$(echo "$json_data" | jq -r '.[].login.username')
+# Extract the list of usernames and Bitwarden names from the JSON
+usernames_and_names=$(echo "$json_data" | jq -r '.[] | "\(.login.username) (\(.name))"')
 
-# Pipe the list of usernames to fzf and retrieve the selected username
-selected_username=$(echo "$usernames" | fzf)
+# Pipe the list of usernames and Bitwarden names to fzf and retrieve the selected entry
+selected_entry=$(echo "$usernames_and_names" | fzf)
+
+# Extract the selected username and Bitwarden name
+selected_username=$(echo "$selected_entry" | awk '{print $1}')
 
 # Find the corresponding password for the selected username
 password=$(echo "$json_data" | jq -r --arg username "$selected_username" '.[] | select(.login.username == $username) | .login.password')
@@ -18,11 +22,14 @@ if [ -n "$password" ]; then
   username=$(echo "$selected_username" | cut -d "@" -f 1)
   host=$(echo "$selected_username" | cut -d "@" -f 2)
 
-  # Echo the sshpass command
-  sshpass -p "$password" ssh "$username@$host"
+  # Echo the sshpass command with Bitwarden name
+  sshpass -p "$password" ssh -o StrictHostKeyChecking=no "$username@$host"
 
   # Uncomment the line below to actually execute the sshpass command
   # sshpass -p "$password" ssh "$username@$host"
 else
   echo "Error: Password not found for the selected username."
 fi
+
+# Display the Bitwarden name
+echo "Bitwarden Name: $selected_bw_name"
